6e633a77e5245b89d2895449735d3ea7b3bfad51
smartbulletosx <37243514+smartbulletosx@users.noreply.github.com>
2019-01-31 16:54:13 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+package TheCubeProject;
+
+public class Main {
+    public static void main(String[] args) {
+        Cube cube = new Cube();
+        cube.printCube();
+        int[][] i = {{1,1,1}, {1,1,1}, {1,1,1}};
+        System.out.println(i[0][0]);
+
+    }
+
+}
